06f7c420a661547fd25da37c1c92fa86538d9c04
jxcv0 <jmicoud02@gmail.com>
2023-04-17 12:03:54 +0100

    fix operator*

--- matrix.h
+++ matrix.h
@@ -1,30 +1,32 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
-#include <cstddef>
 #include <array>
+#include <cassert>
 #include <cmath>
+#include <cstddef>
+#include <ostream>
 
-bool feq(float a, float b) {
-  return fabs(a - b) < FLT_EPSILON;
-}
+bool feq(float a, float b) { return fabs(a - b) < FLT_EPSILON; }
 
 template <std::size_t N, std::size_t M>
-class mat {
+struct mat {
  public:
   mat() = default;
 
-  auto operator[](const int i) {
+  auto &operator[](const std::size_t i) {
+    assert(i < N);
     return m_elem[i];
   }
 
-  const auto operator[](const int i) const {
+  const auto &operator[](const std::size_t i) const {
+    assert(i < N);
     return m_elem[i];
   }
 
   template <std::size_t K>
   friend mat<N, K> operator*(const mat<N, M> &m1, const mat<M, K> &m2) {
-    mat<N, K> result;
+    mat<N, K> result{};
     for (std::size_t n = 0; n < N; n++) {
       for (std::size_t m = 0; m < M; m++) {
         for (std::size_t k = 0; k < K; k++) {
@@ -46,8 +48,31 @@ class mat {
     return true;
   }
 
- private:
-  float m_elem[N][M];
+  friend bool operator!=(const mat<N, M> &m1, const mat<N, M> &m2) {
+    for (std::size_t n = 0; n < N; n++) {
+      for (std::size_t m = 0; m < M; m++) {
+        if (feq(m1[m][n], m2[m][n])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const mat<N, M> &mat) {
+    os << "{ ";
+    for (std::size_t n = 0; n < N; n++) {
+      os << "{ ";
+      for (std::size_t m = 0; m < M; m++) {
+        os << mat[n][m] << " ";
+      }
+      os << "} ";
+    }
+    os << "} ";
+    return os;
+  }
+
+  std::array<std::array<float, M>, N> m_elem;
 };
 
 template <std::size_t M>
--- matrix_tests.cpp
+++ matrix_tests.cpp
@@ -11,18 +11,18 @@ TEST(matrix_tests, subscript) {
 }
 
 TEST(matrix_tests, equal) {
-  mat<2, 2> m1;
-  m1[0][0] = 10.0f;
-
-  mat<2, 2> m3{{3,2,3,4}};
+  mat<2, 2> m1{1, 2, 3, 4};
+  mat<2, 2> m2{1, 2, 3, 4};
   ASSERT_TRUE(m1 == m2);
-  ASSERT_FALSE(m1 == m3);
+  m2[1][1] = -0.1f;
+  ASSERT_FALSE(m1 == m2);
 }
 
 TEST(matrix_tests, dot) {
-  mat<2, 3> b{{1, 2, 3, 4, 5, 6}};
-  mat<3, 2> a{{1, 2, 3, 4, 5, 6}};
+  mat<3, 2> a{1, 2, 3, 4, 5, 6};
+  mat<2, 3> b{1, 2, 3, 4, 5, 6};
   auto c = a * b;
+  mat<3, 3> r{9, 12, 15, 19, 26, 33, 29, 40, 51};
 
-  mat<3, 3> r{{9, 12, 15, 19, 26, 33, 29, 33, 29, 40, 51}};
+  ASSERT_TRUE(c == r);
 }
